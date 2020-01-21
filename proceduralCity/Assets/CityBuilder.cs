ede2b71a959b0f3ab73c72daa0bd2bfea483c9e5
Rita <rita.martinif@gmail.com>
2020-01-21 20:53:06 +0000

    first city generation

---
+++ proceduralCity/Assets/CityBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityBuilder : MonoBehaviour
+{
+    public GameObject[] buildings;
+    public int mapWidth = 20;
+    public int mapHeight = 20;
+    int buildingsSpacing = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float seed = Random.Range(0,100);
+        for(int y = 0; y < mapHeight;y++)
+        {
+            for(int i = 0;i < mapWidth; i++)
+            {
+                int result = (int) (Mathf.PerlinNoise(i/10.0f + seed ,y/10.0f +seed) * 10);
+                Vector3 pos = new Vector3(i * buildingsSpacing, 0, y * buildingsSpacing);
+                if (result < 2)
+                    Instantiate(buildings[0], pos, Quaternion.identity);
+                else if (result<4)
+                    Instantiate(buildings[1], pos, Quaternion.identity);
+                else if (result < 6)
+                    Instantiate(buildings[2], pos, Quaternion.identity);
+                else if (result < 8)
+                    Instantiate(buildings[3], pos, Quaternion.identity);
+                else if (result < 10)
+                    Instantiate(buildings[4], pos , Quaternion.Euler(0,-90,0));
+
+            }
+        }
+    }
+
+   
+}
